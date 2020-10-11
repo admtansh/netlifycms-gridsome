606327b85a87f93d0207e648f6dce2b01d081676
admtansh <admtansh@users.noreply.github.com>
2020-10-11 23:18:48 +0530

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Netlify CMS Gridsome starter template',
+  siteName: 'TANSH SKINCARE',
   siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Netlify CMS for content.',
 
   templates: {
